87a86ea125a1237d870154bced4b2aa4addca46d
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-04-07 09:39:13 +0200

    Misc updates.

--- mr_transform/mr_transform_wrapper_fits_to_png_planes.py
+++ mr_transform/mr_transform_wrapper_fits_to_png_planes.py
@@ -19,18 +19,21 @@ MR_OUTPUT_FILE_PATH = "out.fits"
 
 parser = argparse.ArgumentParser(description="MrTransform wrapper.")
 
+parser.add_argument("--number_of_scales", "-n", type=int, default=4, metavar="INTEGER",
+                    help="number of scales used in the multiresolution transform (default: 4)")
 parser.add_argument("filearg", nargs=1, metavar="FILE",
                     help="the FITS file to process")
 
 args = parser.parse_args()
 
+number_of_scales = args.number_of_scales
 input_file_path = args.filearg[0]
 
 
 # EXECUTE MR_TRANSFORM ########################################################
 
 # TODO: improve the following lines
-cmd = 'mr_transform -n5 "{}" out'.format(input_file_path)
+cmd = 'mr_transform -n{} "{}" out'.format(number_of_scales, input_file_path)
 os.system(cmd)
 
 # TODO: improve the following lines
@@ -43,17 +46,19 @@ os.system(cmd)
 # Open the FITS file
 hdu_list = fits.open(MR_OUTPUT_FILE_PATH)
 
-for hdu_id, hdu in enumerate(hdu_list):             # TODO: ONLY ONE HDU EXPECTED ???
-    data = hdu.data   # "hdu.data" is a Numpy Array
+if len(hdu_list) != 1:
+    raise Exception("Unexpected error: the output FITS file should contain only one HDU.")
 
-    if data.ndim != 3:
-        raise Exception("The input FITS file should contain only one (2D) image.")
+data = hdu_list[0].data   # "hdu.data" is a Numpy Array
 
-    for img_index, img in enumerate(data):
-        fig = plt.figure(figsize=(8.0, 8.0))
-        ax = fig.add_subplot(111)
-        ax.imshow(img, interpolation='nearest', cmap=cm.gray)
-        plt.savefig("HDU{}_{}.png".format(hdu_id, img_index))
+if data.ndim != 3:
+    raise Exception("Unexpected error: the output FITS file should contain a 3D array.")
+
+for img_index, img in enumerate(data):
+    fig = plt.figure(figsize=(8.0, 8.0))
+    ax = fig.add_subplot(111)
+    ax.imshow(img, interpolation='nearest', cmap=cm.gray)
+    plt.savefig("mr_{}.png".format(img_index))
 
 # Close the FITS file
 hdu_list.close()
