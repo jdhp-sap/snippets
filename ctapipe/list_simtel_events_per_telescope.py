61204dd4d4a212fc1e473923fbd76018231b3201
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-05-26 17:23:21 +0200

    Misc updates.

--- ctapipe/list_simtel_events_per_telescope.py
+++ ctapipe/list_simtel_events_per_telescope.py
@@ -29,11 +29,11 @@ def list_simtel_events_per_telescope(simtel_file_path):
     events_per_tel_dict = {}   # List of events per telescope
 
     for event in source:
-        triggered_telescopes_list = [int(tel_id) for tel_id in event["trig"]["tels_with_trigger"]]
+        triggered_telescopes_list = [int(tel_id) for tel_id in event.trig.tels_with_trigger]
         for telescope_id in triggered_telescopes_list:
             if telescope_id not in events_per_tel_dict:
                 events_per_tel_dict[telescope_id] = []
-            events_per_tel_dict[telescope_id].append(int(event["dl0"]["event_id"]))
+            events_per_tel_dict[telescope_id].append(int(event.dl0.event_id))
 
     return events_per_tel_dict
 
