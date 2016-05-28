3d5792a5c1d85b809efc2aaf3301ec586faed6e3
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-05-28 16:42:59 +0200

    Add a meta script.

---
+++ ctapipe/xp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+INPUT_DIR=~/data/2016_05_19/sim_006/
+
+################################################################################
+
+./plot_all_common_events.sh ${INPUT_DIR}/*cta-prod3-demo_desert-2150m-Paranal-n*.gz
+
+mkdir default
+mv *.pdf default/
+mv *.json default/
+
+################################################################################
+
+./plot_all_common_events.sh ${INPUT_DIR}/*cta-prod3-demo_desert-2150m-Paranal-c*.gz
+
+mkdir nonoise
+mv *.pdf nonoise/
+mv *.json nonoise/
+
+################################################################################
+
+./plot_all_common_events.sh ${INPUT_DIR}/*cta-prod3-demo_desert-2150m-Paranal-n*.gz
+
+mkdir almost_nonoise
+mv *.pdf almost_nonoise/
+mv *.json almost_nonoise/
+
