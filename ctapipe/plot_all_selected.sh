116ea3e844f9330ab2962a27d9f5e1386f3c858c
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-05-30 16:13:31 +0200

    Simplify a snippet.

--- ctapipe/plot_all_selected.sh
+++ ctapipe/plot_all_selected.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
 
-SELECTED_EVENTID=312
-SELECTED_TELID=7
+EVENTID=
+TELID=
 
 for SIMTEL_FILE in "$@"
 do
-    echo "${SIMTEL_FILE}"
+    OUTPUT_FILE_NAME="${SIMTEL_FILE}_TEL${TELID}_EV${EVENTID}_CH0"
+    echo "${OUTPUT_FILE_NAME}"
 
-    ./list_events.py ${SIMTEL_FILE} | while read -r LINE
-    do
-        RESULTARRAY=($LINE)
-
-        EVENTID=${RESULTARRAY[0]}
-        TELID=${RESULTARRAY[1]}
-
-        if [ $EVENTID = ${SELECTED_EVENTID} ] && [ $TELID = ${SELECTED_TELID} ]
-        then
-            OUTPUT_FILE_NAME="${SIMTEL_FILE}_TEL${TELID}_EV${EVENTID}_CH0"
-            echo "${OUTPUT_FILE_NAME}"
-
-            #echo "TELID=${TELID} EVENTID=${EVENTID}"
-            ./plot_events_image.py -q -o "${OUTPUT_FILE_NAME}.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
-            ./plot_events_photoelectron_image.py -q -o "${OUTPUT_FILE_NAME}_PE.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
-            ./plot_events_image_histogram.py -q -o "${OUTPUT_FILE_NAME}_HIST.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
-            ./plot_events_photoelectron_image_histogram.py -q -o "${OUTPUT_FILE_NAME}_PE_HIST.pdf" -t $TELID -e "$EVENTID ${SIMTEL_FILE}" 2> /dev/null
-            ./event_photoelectron_image_to_json.py -o "${OUTPUT_FILE_NAME}_PE.json" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
-            ./event_image_to_json.py -o "${OUTPUT_FILE_NAME}.json" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
-        fi
-    done
-
+    #echo "TELID=${TELID} EVENTID=${EVENTID}"
+    ./plot_events_image.py -q -o "${OUTPUT_FILE_NAME}.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+    ./plot_events_photoelectron_image.py -q -o "${OUTPUT_FILE_NAME}_PE.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+    ./plot_events_image_histogram.py -q -o "${OUTPUT_FILE_NAME}_HIST.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+    ./plot_events_photoelectron_image_histogram.py -q -o "${OUTPUT_FILE_NAME}_PE_HIST.pdf" -t $TELID -e "$EVENTID ${SIMTEL_FILE}" 2> /dev/null
+    ./event_photoelectron_image_to_json.py -o "${OUTPUT_FILE_NAME}_PE.json" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+    ./event_image_to_json.py -o "${OUTPUT_FILE_NAME}.json" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
 done
