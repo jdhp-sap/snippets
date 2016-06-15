e54a023c8613ea1842aed10a3064be26265f1860
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-06-15 11:51:13 +0200

    Add a snippet.

---
+++ ctapipe/plot_all_astri.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for SIMTEL_FILE in "$@"
+do
+    echo "${SIMTEL_FILE}"
+
+    ./list_events.py ${SIMTEL_FILE} | while read -r LINE
+    do
+        RESULTARRAY=($LINE)
+
+        EVENTID=${RESULTARRAY[0]}
+        TELID=${RESULTARRAY[1]}
+
+        if [ $TELID -ge 29 -a $TELID -le 52 ]
+        then
+            OUTPUT_FILE_NAME="${SIMTEL_FILE}_TEL${TELID}_EV${EVENTID}_CH0"
+            echo "${OUTPUT_FILE_NAME}"
+
+            #echo "TELID=${TELID} EVENTID=${EVENTID}"
+            ./extract_and_crop_simtel_images.py -o "${OUTPUT_FILE_NAME}.fits" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+            ./plot_events_image.py -q -o "${OUTPUT_FILE_NAME}.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+            ./plot_events_photoelectron_image.py -q -o "${OUTPUT_FILE_NAME}_PE.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+        fi
+    done
+
+done
