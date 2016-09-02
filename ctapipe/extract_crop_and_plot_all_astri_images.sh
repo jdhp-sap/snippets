216456552984905bd720dc131f24c0e983c49bee
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-09-02 15:49:41 +0200

    Misc updates.

--- ctapipe/extract_crop_and_plot_all_astri_images.sh
+++ ctapipe/extract_crop_and_plot_all_astri_images.sh
@@ -11,16 +11,16 @@ do
         EVENTID=${RESULTARRAY[0]}
         TELID=${RESULTARRAY[1]}
 
-        if [ $TELID -ge 29 -a $TELID -le 52 ]
+        if [ $TELID -ge 1 -a $TELID -le 33 ]
         then
             OUTPUT_FILE_NAME="${SIMTEL_FILE}_TEL${TELID}_EV${EVENTID}_CH0"
             echo "${OUTPUT_FILE_NAME}"
 
             #echo "TELID=${TELID} EVENTID=${EVENTID}"
             ./extract_and_crop_simtel_images.py -o "${OUTPUT_FILE_NAME}.fits" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
-            ./plot_events_image.py -q -o "${OUTPUT_FILE_NAME}.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
-            ./plot_events_calibrated_image.py -q -o "${OUTPUT_FILE_NAME}.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
-            ./plot_events_photoelectron_image.py -q -o "${OUTPUT_FILE_NAME}_PE.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+            #./plot_events_image.py -q -o "${OUTPUT_FILE_NAME}.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+            #./plot_events_calibrated_image.py -q -o "${OUTPUT_FILE_NAME}.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
+            #./plot_events_photoelectron_image.py -q -o "${OUTPUT_FILE_NAME}_PE.pdf" -t $TELID -e $EVENTID "${SIMTEL_FILE}" 2> /dev/null
         fi
     done
 
